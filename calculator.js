d7872e2326fc3aee774158a3b68c9780d4802232
patshikwe <patshikwe@yahoo.fr>
2023-02-25 04:46:39 +0100

    Création de la fenêtre modale

--- calculator.js
+++ calculator.js
@@ -1,7 +1,6 @@
 
 const container = document.querySelector(".container");
 
-// let fristName = window.prompt("Bienvenue! Entrez votre Prénom,s'il vous plait.","");
 
 const createElements = () => {
     // Création de div pour la salutation
@@ -14,14 +13,41 @@ const createElements = () => {
     divCalculator.className = "calculator";
     container.appendChild(divCalculator);
 
-    // creation de fênetre modale pour récupérer les cordonnées de l'utilisateur
+    // creation de fenêtre modale pour récupérer les cordonnées de l'utilisateur
     const modalWindow = document.createElement("div");
     modalWindow.className = "modal-window";
-    container.appendChild(modalWindow);
-    
+    container.appendChild(modalWindow); 
 }
 
+// Composition de la fenêtre modale
+const handleModalWindow = (el) => {
+    const getModalWindow = document.querySelector(".modal-window");
+
+
+    // Création input Prénom et paragraphe
+    const paraFirstName = document.createElement("p");
+    getModalWindow.appendChild(paraFirstName);
+    paraFirstName.innerText = "Entrez votre Prénom";
+
+    const inputFirstName = document.createElement("input");
+    inputFirstName.type = "text";
+    inputFirstName.name = "firstName";
+    getModalWindow.appendChild(inputFirstName);
+
+    // Création input Nom et paragraphe
+    const paraLastName = document.createElement("p");
+    getModalWindow.appendChild(paraLastName);
+    paraLastName.innerText = "Entrez votre Nom";
+
+    const inputName = document.createElement("input");
+    inputName.type = "text";
+    inputName.name = "lastName";
+    getModalWindow.appendChild(inputName);
+
+    // console.log(inputFirstName.value, inputName.value);
+}
 
 function init() {
-    createElements()
+    createElements();
+    handleModalWindow();
 }
